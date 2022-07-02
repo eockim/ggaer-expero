eba6fe03a47d7855986968b5ede463188323d3a4
eockim <hyungjoon6876@gmail.com>
2022-07-02 11:08:27 +0900

    init

---
+++ src/main/kotlin/com/ggaer/ggaerexpero/GgaerExperoApplication.kt
@@ -0,0 +1,11 @@
+package com.ggaer.ggaerexpero
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GgaerExperoApplication
+
+fun main(args: Array<String>) {
+	runApplication<GgaerExperoApplication>(*args)
+}
---
+++ src/test/kotlin/com/ggaer/ggaerexpero/GgaerExperoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ggaer.ggaerexpero
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GgaerExperoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
